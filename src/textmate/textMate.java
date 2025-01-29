bf5fd6d9948640513eac3de69a3676a2463731f5
Giacomo <Giacomo@DESKTOP-7DOP2V9.home-life.hub>
2025-01-29 13:37:10 +0100

    definito il caching opzionale degli Executable in funzione della firma
    definito boolean cache in Utility
    definito record Signature in Utility
    definito hashmap<Signature,Executable> in Utility
    rivisto metodo getExecutable in Utility
    
    rinominato $nm segnaposto fn nei boot ed in condSys.lispx
    adeguato reference.lsp
    
    definita directory src/textmate
    spostato language-configuration.json in src/textmate
    definito tmLanguage.json ed example.wat in src/textmate
    definito prova.java in src/textmate
    definito prova.java in src/textmate
    definito textMate.java in src/textMate
    definito Lisp.css in src/textMate

---
+++ src/textmate/textMate.java
@@ -0,0 +1,29 @@
+package textmate;
+
+import java.nio.file.Path;
+
+import org.eclipse.tm4e.core.grammar.IGrammar;
+import org.eclipse.tm4e.core.grammar.IToken;
+import org.eclipse.tm4e.core.grammar.ITokenizeLineResult;
+import org.eclipse.tm4e.core.registry.IGrammarSource;
+import org.eclipse.tm4e.core.registry.Registry;
+
+public class textMate {
+	public static void main(String[] args) {
+		Registry registry = new Registry();
+		var grammarSource = IGrammarSource.fromFile(Path.of("src/textmate/tmLanguage.json"));
+		IGrammar grammar = registry.addGrammar(grammarSource);
+		var str = "(def ab 1 (2) \"sdsd\" #\\x28 #\\x #\\  '(1 2) 'abc)";
+		ITokenizeLineResult lineTokens = grammar.tokenizeLine(str);
+		IToken[] tokens = (IToken[]) lineTokens.getTokens();
+		for (int i = 0; i < tokens.length; i++) {
+			IToken token = tokens[i];
+			token.getScopes().add(str.substring(token.getStartIndex(), token.getEndIndex()));
+			System.out.println(
+				"Token from " + token.getStartIndex()
+				+ " to " + token.getEndIndex()
+				+ " with scopes " + token.getScopes()
+			);
+		}
+	}
+}
